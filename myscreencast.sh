5dff769aef9593e8599cf4584e291ea0dad685ef
nicolargo <nicolas@nicolargo.com>
2010-04-23 21:12:01 +0000

    GStreamer only to encode

--- myscreencast.sh
+++ myscreencast.sh
@@ -12,10 +12,10 @@
 # Auteur: Nicolas Hennion aka Nicolargo
 # GPL v3
 # 
-VERSION="0.2"
+VERSION="0.3"
 
 ### Variables
-WEBCAM="/dev/video1"
+WEBCAM="/dev/video0"
 OUTPUTHEIGHT=720
 OUTPUTFPS=10
 ###
@@ -27,14 +27,29 @@ OUTPUTWIDTH=$(echo "$SOURCEWIDHT * $OUTPUTHEIGHT / $SOURCEHEIGHT" | bc)
 
 encode() {
   echo "ENCODAGE THEORA/VORBIS EN COURS: screencast-$DATE.ogg"
-  gst-launch filesrc location=screencast.yuv ! videoparse format=3 width=$OUTPUTWIDTH height=$OUTPUTHEIGHT framerate=$OUTPUTFPS/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesink location=screencast.ogv 2>&1 >>/dev/null
-  oggJoin screencast-$DATE.ogg screencast.ogv screencast.oga
+  #gst-launch filesrc location=screencast.yuv ! videoparse format=3 width=$OUTPUTWIDTH height=$OUTPUTHEIGHT framerate=$OUTPUTFPS/1 ! ffmpegcolorspace ! theoraenc ! oggmux ! filesink location=screencast.ogv 2>&1 >>/dev/null
+  #oggJoin screencast-$DATE.ogg screencast.ogv screencast.oga
+  gst-launch oggmux name=mux ! filesink location=screencast.ogg \
+	filesrc location=screencast.yuv \
+	! videoparse format=3 width=$OUTPUTWIDTH height=$OUTPUTHEIGHT framerate=$OUTPUTFPS/1 \
+	! queue ! ffmpegcolorspace ! theoraenc ! queue ! mux. \
+	filesrc location=screencast.oga \
+	! decodebin \
+	! queue ! audioconvert ! vorbisenc ! queue ! mux.
+
 
   echo "ENCODAGE H.264/AAC EN COURS: screencast-$DATE.mp4"
   # Quand x264 supportera conteneur mp4, encoder à partir de yuv avec x264
-  ffmpeg -i screencast-$DATE.ogg -vcodec libx264 -vpre hq -crf 20 -acodec aac -f mp4 -threads 0 screencast-$DATE.m4v
+  #ffmpeg -i screencast-$DATE.ogg -vcodec libx264 -vpre hq -crf 20 -acodec aac -f mp4 -threads 0 screencast-$DATE.m4v
+  gst-launch ffmux_mp4 name=mux ! filesink location=screencast.mp4 \
+	filesrc location=screencast.yuv \
+	! videoparse format=3 width=$OUTPUTWIDTH height=$OUTPUTHEIGHT framerate=$OUTPUTFPS/1 \
+	! queue ! ffmpegcolorspace ! x264enc ! queue ! mux. \
+	filesrc location=screencast.oga \
+	! decodebin \
+	! queue ! audioconvert ! faac ! queue ! mux.
   
-  rm -f screencast.oga screencast.ogv screencast.yuv
+  rm -f screencast.oga screencast.yuv
   echo "FIN"
   exit 1
 }
