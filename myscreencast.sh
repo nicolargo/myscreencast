dba90e77001d7b03df7409f12a8c6279de70ba97
nicolargo <nicolas@nicolargo.com>
2010-04-28 15:00:49 +0000

    Utilisation fichier avi mux audio + video en entree

--- myscreencast.sh
+++ myscreencast.sh
@@ -11,12 +11,13 @@
 # Auteur: Nicolas Hennion aka Nicolargo
 # GPL v3
 # 
-VERSION="0.4"
+VERSION="0.5"
 
 ### Variables à ajuster selon votre configuration
-WEBCAMDEVICE="/dev/video1"
+#AUDIODEVICE="gconfaudiosrc"
+AUDIODEVICE="alsasrc"
+WEBCAMDEVICE="/dev/video0"
 WEBCAMHEIGHT="240"
-AUDIOAMPLI="1"
 OUTPUTHEIGHT="720"
 OUTPUTFPS="10"
 ### Fin des variables à ajuster
@@ -32,25 +33,20 @@ AACENC="faac tns=true"
 
 encode() {
   echo "ENCODAGE THEORA/VORBIS EN COURS: screencast-$DATE.ogg"
-  gst-launch oggmux name=mux ! filesink location=screencast-$DATE.ogg \
-	filesrc location=screencast.yuv \
-	! videoparse format=3 width=$OUTPUTWIDTH height=$OUTPUTHEIGHT framerate=$OUTPUTFPS/1 \
-	! queue ! ffmpegcolorspace ! $THEORAENC ! queue ! mux. \
-	filesrc location=screencast.wav \
-	! decodebin \
-	! queue ! audioconvert ! $VORBISENC ! queue ! mux. 2>&1 >>/dev/null
-
+  gst-launch filesrc location=screencast.avi ! decodebin name="decode" \
+    decode. ! videoparse format=1 width=$OUTPUTWIDTH height=$OUTPUTHEIGHT framerate=$OUTPUTFPS/1 \
+	! queue ! ffmpegcolorspace ! $THEORAENC ! queue ! \
+    oggmux name=mux ! filesink location=screencast-$DATE.ogg \
+    decode. ! queue ! audioconvert ! $VORBISENC ! queue ! mux.
 
   echo "ENCODAGE H.264/AAC EN COURS: screencast-$DATE.mp4"
-  gst-launch ffmux_mp4 name=mux ! filesink location=screencast-$DATE.mp4 \
-	filesrc location=screencast.yuv \
-	! videoparse format=3 width=$OUTPUTWIDTH height=$OUTPUTHEIGHT framerate=$OUTPUTFPS/1 \
-	! queue ! ffmpegcolorspace ! $H264ENC ! queue ! mux. \
-	filesrc location=screencast.wav \
-	! decodebin \
-	! queue ! audioconvert ! $AACENC ! queue ! mux. 2>&1 >>/dev/null
+  gst-launch filesrc location=screencast.avi ! decodebin name="decode" \
+    decode. ! videoparse format=1 width=$OUTPUTWIDTH height=$OUTPUTHEIGHT framerate=$OUTPUTFPS/1 \
+    ! queue ! ffmpegcolorspace ! $H264ENC ! queue ! \
+    ffmux_mp4 name=mux ! filesink location=screencast-$DATE.mp4 \
+    decode. ! queue ! audioconvert ! $AACENC ! queue ! mux.
   
-  rm -f screencast.wav screencast.yuv
+  rm -f screencast.avi
   echo "FIN DE LA CAPTURE"
   exit 1
 }
@@ -64,6 +60,9 @@ sleep 3
 trap encode 1 2 3 6
 echo "AUDIO: ON"
 echo "CAPTURE EN COURS (CTRL-C pour arreter)"
-gst-launch gconfaudiosrc name=audiosource ! audioconvert ! audioamplify amplification=$AUDIOAMPLI ! wavenc ! filesink location=screencast.wav 2>&1 >>/dev/null &
-gst-launch istximagesrc name=videosource use-damage=false ! video/x-raw-rgb,framerate=$OUTPUTFPS/1 ! videorate ! ffmpegcolorspace ! videoscale method=1 ! video/x-raw-yuv,width=$OUTPUTWIDTH,height=$OUTPUTHEIGHT,framerate=$OUTPUTFPS/1 ! filesink location=screencast.yuv 2>&1 >>/dev/null
-
+gst-launch avimux name=mux ! filesink location=screencast.avi \
+	$AUDIODEVICE ! audioconvert ! queue ! mux. \
+	istximagesrc name=videosource use-damage=false ! video/x-raw-rgb,framerate=$OUTPUTFPS/1 \
+	! ffmpegcolorspace ! queue ! videorate ! ffmpegcolorspace ! videoscale method=1 \
+	! video/x-raw-yuv,width=$OUTPUTWIDTH,height=$OUTPUTHEIGHT,framerate=$OUTPUTFPS/1 ! mux.
+	
